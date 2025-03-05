37093291b6ca4d2fe343f8e5a3549ef1e839962d
Luis6-uem <luisyustedeagustin@gmail.com>
2025-03-05 12:30:35 +0100

    Ejercicio3.py

--- colecciones-listas/Tuplas/Ejercicio3.py
+++ colecciones-listas/Tuplas/Ejercicio3.py
@@ -1,4 +1,3 @@
-tupla_vacia = ()  # Una tupla es inmutable, no se pueden agregar valores directamente.
-# Para modificarla, hay que redefinirla:
+tupla_vacia = ()
 tupla_vacia = ("Nuevo valor",)
 print(tupla_vacia)
